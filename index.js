a5823a1266e0b07b364da063ca81b1be94f34cd7
Miguel Juarez <hippothesign@gmail.com>
2016-12-03 07:32:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+  array.forEach(element, index) => {
+  console.log(`${index}: ${element}`)
+})
+}
+
+function iterate(callback){
+  var candy = ['Skittles', 'Starburst', 'Sweedish Fish']
+  candy.forEach(callback)
+  return candy
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
